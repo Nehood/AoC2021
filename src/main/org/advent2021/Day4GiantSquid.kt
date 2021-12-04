0a4266af5ffbb719f0f840e16470a03328d01353
Nehood <gorkiewiczm@gmail.com>
2021-12-04 23:22:59 +0100

    Day 4 solved

--- src/main/org/advent2021/Day4GiantSquid.kt
+++ src/main/org/advent2021/Day4GiantSquid.kt
@@ -1,23 +1,54 @@
 package org.advent2021
 
+import org.advent2021.data.Day4BingoBoard
+
 class Day4GiantSquid {
 
-    val BOARD_SIZE = 5
-
-    fun playBingo(input: List<String>): Int{
-        //input.forEach { println(it) }
+    fun playBingo(input: List<String>): Int {
         val numbers = input[0].split(',').map { it.toInt() }
 
         val bingos = input
-                .drop(1)    // skip first element (numbers)
+                .drop(1)
                 .filter { it.isNotEmpty() }
-                .chunked(BOARD_SIZE)    // size +1  because we are also reading emptyline
-        bingos.forEach {
-            it.forEach {
-                print("$it ")
+                .chunked(Day4BingoBoard.BOARD_SIZE)
+                .map { Day4BingoBoard(it) }
+
+        for (number in numbers) {
+            for (board in bingos) {
+                if (board.bingo(number)) {
+                    return board.getSumOfUnmarked() * number
+                }
             }
-            println("next")
         }
-        return 0
+        return -1
+    }
+
+    fun playBingoLetSquidWin(input: List<String>): Int {
+        val numbers = input[0].split(',').map { it.toInt() }
+
+        val bingos = input
+                .drop(1)
+                .filter { it.isNotEmpty() }
+                .chunked(Day4BingoBoard.BOARD_SIZE)
+                .map { Day4BingoBoard(it) }
+                .toMutableList()
+
+        for (number in numbers) {
+            val wonBingos = mutableListOf<Day4BingoBoard>()
+            if (bingos.size > 1) {
+                for (board in bingos) {
+                    if (board.bingo(number)) {
+                        wonBingos.add(board)
+                    }
+                }
+            } else {
+                if (bingos[0].bingo(number)) {
+                    return bingos[0].getSumOfUnmarked() * number
+                }
+
+            }
+            bingos.removeAll(wonBingos)
+        }
+        return -1
     }
 }
--- src/main/org/advent2021/data/Day4BingoBoard.kt
+++ src/main/org/advent2021/data/Day4BingoBoard.kt
@@ -2,20 +2,63 @@ package org.advent2021.data
 
 class Day4BingoBoard {
 
+    companion object {
+        var BOARD_SIZE = 5
+    }
+
     private val bingoBoard: Array<IntArray>
     private val numbers: Map<Int, Boolean>
+    private val markedNumbers = mutableMapOf<Int, Boolean>()
 
     constructor(board: List<String>) {
-        val currentBoard = Array(5) { IntArray(5) }
+        val currentBoard = Array(BOARD_SIZE) { IntArray(BOARD_SIZE) }
         val currentNumbers = mutableMapOf<Int, Boolean>()
         for ((y, row) in board.withIndex()) {
-            row.map { it.toInt() }.forEachIndexed { x, i ->
-                currentBoard[y][x] = i
-                currentNumbers[i] = true
-            }
+            row.replace("  ", " ")  // dirty hack for 1 number values
+                    .split(' ')
+                    .map { it.toInt() }
+                    .forEachIndexed { x, i ->
+                        currentBoard[y][x] = i
+                        currentNumbers[i] = true
+                    }
         }
 
         bingoBoard = currentBoard.clone()
         numbers = currentNumbers.toMap()
+    }
+
+    fun bingo(number: Int): Boolean {
+        if (!numbers.containsKey(number)) return false
+        markedNumbers[number] = true
+        if (markedNumbers.size < 5) return false
+        return checkForBingo()
+    }
+
+    private fun checkForBingo(): Boolean {
+        return checkRows().or(checkColumns())
+    }
+
+    private fun checkRows(): Boolean {
+        return bingoBoard.any { row ->
+            row.all { markedNumbers[it] ?: false }
+        }
+    }
+
+    private fun checkColumns(): Boolean {
+        var bingo = true
+        for (x in 0..4) {
+            for (y in 0..4) {
+                bingo = markedNumbers[bingoBoard[y][x]] ?: false
+                if (!bingo) break
+            }
+            if (bingo) break
+        }
+        return bingo
+    }
+
+    fun getSumOfUnmarked(): Int {
+        val unmarked = numbers.keys.toMutableList()
+        unmarked.removeAll(markedNumbers.keys)
+        return unmarked.sum()
     }
 }
