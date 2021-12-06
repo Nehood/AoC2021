ca41cb436c05fbedd0758f1188ed606c63a66c4e
Nehood <gorkiewiczm@gmail.com>
2021-12-06 21:43:42 +0100

    small update

--- src/main/org/advent2021/Day6Lanternfish.kt
+++ src/main/org/advent2021/Day6Lanternfish.kt
@@ -5,6 +5,16 @@ import java.math.BigInteger
 class Day6Lanternfish {
 
     fun countLanternfishSpawningForNDays(initialState: IntArray, days: Int): BigInteger {
+        val currentState = initialState.toMutableList()
+        for (i in 0 until days){
+            var newCount = 0
+            for (j in 0..currentState.lastIndex){
+                --currentState[j]
+                if (0 == currentState[j]) {
+                    ++newCount
+                }
+            }
+        }
         return 0.toBigInteger()
     }
 }
